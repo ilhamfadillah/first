b116a0333f418ca0ba2266b3c86d186fb0c92e6c
ilham_fadillah <ilhamfadillah1995@gmail.com>
2018-03-08 07:33:35 +0700

    Sesuai Tugas

--- resources/views/sidebar-userpanel.blade.php
+++ resources/views/sidebar-userpanel.blade.php
@@ -1,9 +1,9 @@
 <div class="user-panel">
   <div class="pull-left image">
-    <img src="public/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+    <img src="http://www.vinilfosco.pt/wp-content/uploads/2017/05/menu1-3.png" class="img-circle" alt="User Image">
   </div>
   <div class="pull-left info">
-    <p>Alexander Pierce</p>
+    <p>Ilham Fadillah</p>
     <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
   </div>
 </div>
