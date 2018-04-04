e3e3f91d5bdb920aedb3aaf71678dfc12c8ccd64
ilham_fadillah <ilhamfadillah1995@gmail.com>
2018-04-05 00:22:22 +0700

    fb

--- database/migrations/2018_03_09_143902_create_users_table.php
+++ database/migrations/2018_03_09_143902_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('username');
             $table->string('password');
             $table->string('email');
-            $table->string('phone_number')->nullable()->change();
+            $table->string('phone_number')->nullable();
             $table->timestamps();
         });
     }
