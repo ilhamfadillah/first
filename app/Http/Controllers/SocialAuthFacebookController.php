1e148ac82ee82e1321648570df6d97b3a201db6a
ilham_fadillah <ilhamfadillah1995@gmail.com>
2018-04-05 00:06:47 +0700

    fb

--- app/Http/Controllers/SocialAuthFacebookController.php
+++ app/Http/Controllers/SocialAuthFacebookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Socialite;
-use SocialFacebookAccountService;
+use App\Services\SocialFacebookAccountService;
 
 class SocialAuthFacebookController extends Controller
 {
